483f189175dfa99d2f0c06033fd2b4f07a056e39
丁少华 <960423114@qq.com>
2020-11-21 17:52:33 +0800

    feat: test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-// import customAudio from '@/components/custom-audio/index.vue'; //引入组件
-// export default {
-//   install(Vue: any) {
-//     Vue.component(customAudio.name, customAudio); //注册全局
-//   }
-// };
+import customAudio from '@/components/custom-audio/index.vue'; //引入组件
 
-export default function(){
-  console.log(6665554);
+const main = {
+  install(Vue: any, options: any) {
+    console.log(customAudio.name+'已装载');
+    Vue.component(customAudio.name, customAudio); //注册全局
+  }
+};
+
+// 这里的判断很重要
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(main);
 }
+
+export default main;
