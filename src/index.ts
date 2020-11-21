c9de6a5fbafdc326174fef8bb12478d66609bf7c
丁少华 <960423114@qq.com>
2020-11-21 16:48:07 +0800

    feat: test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import customAudio from '@/components/custom-audio/index.vue'; //引入组件
-export default {
-  install(Vue: any) {
-    Vue.component(customAudio.name, customAudio); //注册全局
-  }
-};
+// import customAudio from '@/components/custom-audio/index.vue'; //引入组件
+// export default {
+//   install(Vue: any) {
+//     Vue.component(customAudio.name, customAudio); //注册全局
+//   }
+// };
+
+export default function(){
+  console.log(8989);
+}
