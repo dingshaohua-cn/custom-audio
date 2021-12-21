7b01673a54528ae5497204924ef5408838f94c6e
丁少华 <dingshaohua@aegis-data.cn>
2021-12-21 11:14:02 +0800

    fix: do something

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-    // 修改 src 目录 为 examples 目录
-    pages: {
-      index: {
-        // page 的入口
-        entry: 'example/main.js',   // 把src 修改为examples
-        // 模板来源
-        template: 'public/index.html',
-        // 在 dist/index.html 的输出
-        filename: 'index.html'
-      }
+  // 修改 src 目录 为 examples 目录
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'example/main.js', // 把src 修改为examples
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
     },
-    css: { extract: false }
-  }
+  },
+  css: { extract: false },
+};
+
+// 发包到npm的时候，注意把package.json里的依赖项全删除，以及script也删除。
